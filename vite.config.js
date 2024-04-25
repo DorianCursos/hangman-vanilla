f95275f67726e62ee79c24d413bd7806e7d62f05
Dorian <doriancursos@gmail.com>
2024-04-25 16:40:57 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
@@ -7,6 +8,15 @@ export default defineConfig({
   base: '',
   root: 'src',
   build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        howToPlay: resolve(__dirname, 'src/html/how-to-play.html'),
+        category: resolve(__dirname, 'src/html/category.html'),
+        game: resolve(__dirname, 'src/html/game.html'),
+        // Añade aquí el resto de páginas que quieras.(nombre único: resolve(__dirname, 'src/html/archivo.html'))
+      },
+    },
     outDir: '../docs',
   },
 });
